2d64531da547ed385433c8d9bd280f274b277a93
Harshit Jain <harshitjain0502@gmail.com>
2025-11-02 19:17:22 +0530

    Implement exit

--- app/main.go
+++ app/main.go
@@ -4,17 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
 func main() {
-	
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		// Wait for user input
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		command_split := strings.Split(command, " ")
+		if command_split[0] == "exit" {
+			exit_code, _ := strconv.Atoi(command_split[1])
+			os.Exit(exit_code)
+		}
 		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
 }
