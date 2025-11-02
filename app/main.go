f0e7688b234a62e90c97f49272630f413b2a8097
Harshit Jain <harshitjain0502@gmail.com>
2025-11-02 20:12:30 +0530

    Locate executable files

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
@@ -11,9 +12,29 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
+func searchCommand(dir string, command string) (bool) {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return false
+	}
+
+	for _, file := range files {
+		if file.Name() == command {
+			fileInfo, _ := file.Info()
+			mode := fileInfo.Mode()
+			if mode&os.FileMode(0111) != 0 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func main() {
 	for {
 		builtin_commands := []string{"exit", "echo", "type"}
+		path_var := os.Getenv("PATH")
+		path_dirs := filepath.SplitList(path_var)
 
 		fmt.Fprint(os.Stdout, "$ ")
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -28,16 +49,27 @@ func main() {
 		} else if command_split[0] == "type" {
 			command_string := strings.Join(command_split[1:], " ")
 			command_string = command_string[:len(command_string)-1]
-			found := false
+			builtin_found := false
 			for _, cmd := range builtin_commands {
 				if cmd == command_string {
 					fmt.Printf("%s is a shell builtin\n", command_string)
-					found = true
+					builtin_found = true
 					break
 				}
 			}
-			if !found {
-				fmt.Printf("%s: not found\n", command_string)
+			if !builtin_found {
+				found := false
+				for _, dir := range path_dirs {
+					if ok := searchCommand(dir, command_string); ok {
+						full_path := filepath.Join(dir, command_string)
+						fmt.Printf("%s is %s\n", command_string, full_path)
+						found = true
+						break
+					}
+				}
+				if !found {
+					fmt.Printf("%s: not found\n", command_string)
+				}
 			}
 		} else {
 			fmt.Printf("%s: command not found\n", command[:len(command)-1])
