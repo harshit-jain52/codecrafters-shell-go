6dc129a894cd6a0a89f8e6c80aaef1b124184613
Harshit Jain <harshitjain0502@gmail.com>
2025-11-02 20:53:01 +0530

    The cd builtin: Absolute paths

--- app/main.go
+++ app/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 var _ = fmt.Fprint
-var builtin_commands = []string{"exit", "echo", "type", "pwd"}
+var builtin_commands = []string{"exit", "echo", "type", "pwd", "cd"}
 
 func searchFileWithPerms(dir string, command string, perms os.FileMode) (bool) {
 	files, err := os.ReadDir(dir)
@@ -45,6 +45,8 @@ func searchCommandInPath(command string) (string, bool){
 }
 
 func main() {
+	current_dir, _ := os.Getwd()
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -74,8 +76,17 @@ func main() {
 				}
 			}
 		} else if command_split[0] == "pwd"{
-			dir, _ := os.Getwd()
-			fmt.Println(dir)
+			fmt.Println(current_dir)
+		} else if command_split[0] == "cd"{
+			absolute_path := command_split[1]
+			fileInfo, err := os.Stat(absolute_path)
+			if err != nil {
+				fmt.Printf("cd: %s: No such file or directory\n", absolute_path)
+			} else if !fileInfo.IsDir(){
+				fmt.Printf("cd: %s: Not a directory\n", absolute_path)
+			} else {
+				current_dir = absolute_path
+			}
 		} else if _ , ok := searchCommandInPath(command_split[0]); ok{
 			args := command_split[1:]
 			cmd := exec.Command(command_split[0], args...)
