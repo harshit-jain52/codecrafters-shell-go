3c60a191c68fc2327c6d217543ee4037624939f6
Harshit Jain <harshitjain0502@gmail.com>
2025-11-02 19:28:44 +0530

    Implement type

--- app/main.go
+++ app/main.go
@@ -13,8 +13,9 @@ var _ = fmt.Fprint
 
 func main() {
 	for {
+		builtin_commands := []string{"exit", "echo", "type"}
+
 		fmt.Fprint(os.Stdout, "$ ")
-		// Wait for user input
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		command_split := strings.Split(command, " ")
 		if command_split[0] == "exit" {
@@ -22,9 +23,24 @@ func main() {
 			os.Exit(exit_code)
 		} else if command_split[0] == "echo" {
 			echoed_string := strings.Join(command_split[1:], " ")
-			fmt.Println(echoed_string[:len(echoed_string)-1])
+			echoed_string = echoed_string[:len(echoed_string)-1]
+			fmt.Println(echoed_string)
+		} else if command_split[0] == "type" {
+			command_string := strings.Join(command_split[1:], " ")
+			command_string = command_string[:len(command_string)-1]
+			found := false
+			for _, cmd := range builtin_commands {
+				if cmd == command_string {
+					fmt.Printf("%s is a shell builtin\n", command_string)
+					found = true
+					break
+				}
+			}
+			if !found {
+				fmt.Printf("%s: not found\n", command_string)
+			}
 		} else {
-			fmt.Println(command[:len(command)-1] + ": command not found")
+			fmt.Printf("%s: command not found\n", command[:len(command)-1])
 		}
 	}
 }
